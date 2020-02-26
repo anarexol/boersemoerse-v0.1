b4201dc1e3769a60011f0b9beb9618a9382345c6
Martin von der Burg <martin@vonderburg.de>
2020-02-26 14:26:16 +0100

    mit aktien

--- BoerseMoerse/BoerseMoerse/Controller/AktuelleKursNotierung.cs
+++ BoerseMoerse/BoerseMoerse/Controller/AktuelleKursNotierung.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoerseMoerse.Model;
 
 namespace BoerseMoerse.Controller
 {
-    static class AktuelleKursNotierung
-    {
+    static class AktuelleKursNotierung {
+        static void AktieHinzufuegen() {
+            new AktienModel() { Name = "Google", AktienID = 0001, Wert = 0 };
+            new AktienModel() { Name = "BitLC", AktienID = 0002, Wert = 0 };
+        }
     }
 }
--- BoerseMoerse/BoerseMoerse/Model/HandelBareAktienModel.cs
+++ BoerseMoerse/BoerseMoerse/Model/HandelBareAktienModel.cs
@@ -9,6 +9,12 @@ namespace BoerseMoerse.Model
 {
     class HandelBareAktienModel
     {
-        List<AktienModel>
+        static List<AktienModel> AktienPool = new List<AktienModel>();
+
+        public static void hinzufuegen(AktienModel aktie) {
+            AktienPool.Add(aktie);
+        }
+        
+
     }
 }
